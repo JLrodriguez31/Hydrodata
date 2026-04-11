d54f3e2de362bc075a9da9b861a5c547f60bb553
JlBestMc <jlrcmt@gmail.com>
2026-04-11 17:23:20 +0200

    vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.tsx'
 import '@/styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
+        <Analytics />
     </StrictMode>
 )
